22f5512423ea5d365ef622fc8c604718fdea4de9
sunanadan4747 <sunandanborse7@gmail.com>
2022-12-03 17:49:33 +0530

    test data done

--- TestData/HomePageData.py
+++ TestData/HomePageData.py
@@ -1,14 +1,14 @@
 import openpyxl
 
 
-class HomePageData:
+class HomePageData:     ## class name has to be pascle type
 
-    test_HomePage_data = [{"firstname":"Rahul","lastname":"shetty","gender":"Male"}, {"firstname":"Anshika", "lastname":"shetty", "gender":"Female"}]
+    test_HomePage_data = [{"firstname":"Sagar","lastname":"Sarade","gender":"Male"}, {"firstname":"Yusuf", "lastname":"Tamboli", "gender":"Male"}]
 
     @staticmethod
-    def getTestData(test_case_name):
+    def getTestData(test_case_name):        # method name has to be camel case
         Dict = {}
-        book = openpyxl.load_workbook("C:/UserData.xlsx")
+        book = openpyxl.load_workbook("../TestData/UserData.xlsx")
         sheet = book.active
         for i in range(1, sheet.max_row + 1):  # to get rows
             if sheet.cell(row=i, column=1).value == test_case_name:
